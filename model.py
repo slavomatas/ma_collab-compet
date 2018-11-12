c2ddd59d533f5c99bbbdfe8c9c2ade4af13b102a
Slavo Matasovsky <slavo.matas@gmail.com>
2018-11-12 20:30:47 +0100

    Changed init params for actor and critic

--- model.py
+++ model.py
@@ -1,7 +1,15 @@
+import numpy as np
 import torch as th
 import torch.nn as nn
+import torch.nn.functional as F
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
 
 
+'''
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
@@ -78,5 +86,72 @@ class Critic(nn.Module):
         if type(m) == nn.Linear:
             nn.init.xavier_uniform_(m.weight)
             m.bias.data.fill_(0.1)
+'''
+
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
+        """Initialize parameters and build model.
+        Params
+        ======
+            input_size (int): number of dimensions for input layer
+            action_size (int): number of dimensions for output layer
+            seed (int): random seed
+            fc1_units (int): number of nodes in first hidden layer
+            fc2_units (int): number of nodes in second hidden layer
+        """
+        super(Actor, self).__init__()
+        self.seed = th.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """Initialize weights with near zero values."""
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, state):
+        """Build an actor (policy) network that maps states -> actions."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = th.tanh(self.fc3(x))
+        return x
 
 
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, n_agents, fc1_units=400, fc2_units=300):
+        """Initialize parameters and build model.
+        Params
+        ======
+            input_size (int): number of dimensions for input layer
+            action_size (int): number of dimensions for output layer
+            n_agents (int): number of agents
+            fc1_units (int): number of nodes in the first hidden layer
+            fc2_units (int): number of nodes in the second hidden layer
+        """
+        super(Critic, self).__init__()
+        self.seed = th.manual_seed(0)
+        self.fc1 = nn.Linear((state_size+action_size) * n_agents, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """Initialize weights with near zero values."""
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+
+    def forward(self, states, actions):
+        """Build a critic (value) network that maps (states, actions) pairs -> Q-values."""
+        xs = th.cat((states, actions), dim=1)
+        x = F.relu(self.fc1(xs))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
